8245ef3978e8a6e3f30daa2af7d09dc09392e18c
Jorge Cascante <j.ksknt@gmail.com>
2026-05-04 18:10:18 -0600

    feat: add S3 and Lambda permissions to CI user for program-builder deployment

--- infra/terraform/bootstrap/modules/iam/main.tf
+++ infra/terraform/bootstrap/modules/iam/main.tf
@@ -26,5 +26,37 @@ resource "aws_iam_user_policy_attachment" "gym_app_ci_ecr" {
 resource "aws_iam_user_policy_attachment" "gym_app_ci_apprunner" {
   user       = aws_iam_user.gym_app_ci.name
   policy_arn = "arn:aws:iam::aws:policy/AWSAppRunnerFullAccess"
-                
+
+}
+
+# Lambda and S3 policy for program-builder deployment
+resource "aws_iam_user_policy" "gym_app_ci_lambda_s3" {
+  name = "gym-app-ci-lambda-s3"
+  user = aws_iam_user.gym_app_ci.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::691650344087-terraform-state",
+          "arn:aws:s3:::691650344087-terraform-state/gym-app/lambda/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:UpdateFunctionCode",
+          "lambda:GetFunction"
+        ]
+        Resource = "arn:aws:lambda:*:*:function:*"
+      }
+    ]
+  })
 }
